9a64d80097f7bd43414cccbb9e58469252732c41
Dada <jt12282575@gmail.com>
2020-05-18 16:48:17 +0800

    [Fix] show STAFF tag when site_admin is true Due to the reponse from HR, the logic of showing STAFF TAG change to show STAFF tag only when site_admin is true.

--- app/src/main/java/dada/com/miraihw/config/Config.kt
+++ app/src/main/java/dada/com/miraihw/config/Config.kt
@@ -2,6 +2,6 @@ package dada.com.miraihw.config
 
 class Config {
     companion object{
-        fun showStaffTag(siteAdmin:Boolean):Boolean = siteAdmin
+        fun showStaffTag(siteAdmin:Boolean):Boolean = !siteAdmin
     }
 }
